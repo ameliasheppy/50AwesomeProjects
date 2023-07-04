422090237b6bdce66b70b0456f9b8d1e7d22634c
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2023-07-04 10:28:10 -0400

    Adding to the repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+  import './App.css';
+  import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //state is info that only this component is aware of and can modify
+  //super is called on the constructor method and any of the other classes you are extending from.
+  constructor(){
+    //call the constructor method on the comp with super()!!!!
+    super();
+
+    //instantiate the state:
+    this.state = {
+      //this.state is alwats a JSON obj
+      name: {firstName: "Jesse", lastName: "Stanley"},
+      company: "ZTM"
+    }
+  }
+  //render is specifically telling the Component what to render
+  render() {
+    return (
+      <div className="App">
+       <h1>Hey there, {this.state.name.firstName} {this.state.name.lastName}. Do you like {this.state.company}</h1>
+       <button onClick={() => {
+        this.setState((state, props) => {
+          return {
+            name: { firstName: "Amy", lastName: "Stanley"}
+          }
+        }, () => {
+          console.log(this.state)
+        })
+       }}>Change Name</button>
+      </div>
+    )
+  }
 }
 
 export default App;
