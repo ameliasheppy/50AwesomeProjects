646d49d837e9b801a556a82c56e20d6a8ab9ac4e
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2024-01-11 22:41:36 -0500

    oops

---
+++ Python100Days/day17.py
@@ -0,0 +1,8 @@
+# Let's make some classes! Need a class to model the webiste we are making, it's our blueprint!
+
+class User:
+    # pass     <- this is a Py keyword that allows us to build a func or class or anything that needs an indentation without an error.
+    pass
+
+
+user_1 = User()
